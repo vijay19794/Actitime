e9318d47a08f3fc4024ba9db35175ce94307aeee
VIJAY RATHORE <VIJAY RATHORE@VIJAY>
2023-05-19 12:52:39 +0530

    createtask

---
+++ Actitime/src/com/actitime/testscript/TaskModule.java
@@ -0,0 +1,11 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TaskModule {
+@Test
+public void testCreateTask() {
+	Reporter.log("CreateTask",true);
+}
+}
